74ac0915c4286ecb3e9b8529401d3b491572a1d0
José Manuel Pereles Pérez <al285986@uji.es>
2017-05-04 20:03:06 +0200

    Structure 1 (get out the first room) done

---
+++ Assets/Code/GameStructure.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStructure : MonoBehaviour {
+
+	public GameObject desk;
+	public GameObject door1;
+	
+	// Update is called once per frame
+	void Update () {
+		if (desk.GetComponent<MakeZoom> ().zoomOut == true) {
+			door1.GetComponentInChildren<CapsuleCollider> ().enabled = true;
+		}
+			
+	}
+}
